dcd8633b5821eb749093dfe957c67d70c36b0e0c
Christopher Shen <mr.chrisshen@gmail.com>
2015-03-19 23:22:48 -0700

    process port

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
+var port = process.env.PORT || 80;
 
-server.listen(80)
+server.listen(port)
 
 app.use("/", express.static(__dirname + '/public'))
 
